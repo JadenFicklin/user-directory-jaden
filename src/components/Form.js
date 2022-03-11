21ddfe6eb122d157fbec7f616523152e0bc1b555
JadenFicklin <jadendficklin@gmail.com>
2022-03-11 10:39:59 -0700

    added styling

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function Form() {
+  // const [name, setName] = useState("");
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(name);
+  // };
+
+  return <></>;
+}
+
+export default Form;
